1869036a65dbbf71ff75bac755e9f02c38a9c922
Max <maxrossfl@yahoo.com>
2025-01-25 13:16:59 -0500

    updated error

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -3,13 +3,17 @@
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 
+type Attribute = "class" | "data-theme" | "data-mode"
+
 interface ThemeProviderProps {
   children: React.ReactNode;
-  attribute?: string;
+  attribute?: Attribute | Attribute[];
   defaultTheme?: string;
   enableSystem?: boolean;
   storageKey?: string;
   themes?: string[];
+  forcedTheme?: string;
+  disableTransitionOnChange?: boolean;
 }
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
